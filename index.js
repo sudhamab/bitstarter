9f3ff67393b49b987571af627ece5f2e9bf1717d
John Smith <john.smith@gmail.com>
2014-09-22 12:30:33 +0000

    read html file and show content

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-var express = require('express')
+var express = require('express');
 var app = express();
+var fs = require('fs');
+var content = fs.readFileSync("index.html");
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send("index.html")
+  response.send(content)
 })
 
 app.listen(app.get('port'), function() {
